17e5991aaab9b98a531575cce768f2c647086031
ketanjain7981 <ketanjain7981@gmail.com>
2025-02-24 12:00:58 +0530

    removed duplicate import

--- backend/core/apps.py
+++ backend/core/apps.py
@@ -4,7 +4,6 @@ from django.apps import AppConfig
 class CoreConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'core'
-    from django.apps import AppConfig
    
     def ready(self):
             import core.signals  # Import signals when the app is ready
